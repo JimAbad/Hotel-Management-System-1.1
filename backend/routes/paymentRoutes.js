e647cd550c217949bae3469db9f8603508878774
PulangRed <redbercasio08@gmail.com>
2025-10-08 18:38:29 +0800

    fix payment

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { confirmPayment, getAllBillings, createPaymentIntent, getPayMongoPaymentDetails, createPaymentMethod } = require('../controllers/paymentController');
+const { confirmPayment, getAllBillings, createPaymentIntent, getPayMongoPaymentDetails, createPaymentMethod, createEWalletPaymentSource } = require('../controllers/paymentController');
 const { protect } = require('../middleware/authMiddleware');
 
 router.post('/create-payment-intent', createPaymentIntent);
 router.post('/create-payment-method', protect, createPaymentMethod);
+router.post('/create-ewallet-payment-source', protect, createEWalletPaymentSource);
 router.post('/confirm', confirmPayment);
 router.get('/my-billings', protect, getAllBillings);
 router.get('/paymongo-details/:bookingId', protect, getPayMongoPaymentDetails);
